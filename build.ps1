c7414f99f082b11d6e6f7aca5d581fb59b4f3dc3
srevrtt <everettsistrunk@icloud.com>
2025-01-13 18:01:43 -0700

    added sdl2_ttf

--- build.ps1
+++ build.ps1
@@ -7,7 +7,7 @@ if ($args[0] -eq "--first") {
     Remove-Item build -Recurse
   }
 
-  New-Item -ItemType directory -Path build
+  $null = New-Item -ItemType directory -Path build
   Set-Location build
   Set-Location ..
 
@@ -15,7 +15,7 @@ if ($args[0] -eq "--first") {
     Remove-Item temp -Recurse
   }
 
-  New-Item -ItemType directory -Path temp
+  $null = New-Item -ItemType directory -Path temp
   Set-Location temp
 
   Write-Host -ForegroundColor "blue" "Downloading SDL2..."
@@ -24,24 +24,40 @@ if ($args[0] -eq "--first") {
   Write-Host -ForegroundColor "blue" "Extracting SDL2..."
   Expand-Archive -Path "sdl2.zip" -DestinationPath "SDL2"
 
+  Write-Host -ForegroundColor "blue" "Downloading SDL2_ttf..."
+  Invoke-WebRequest "https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.24.0/SDL2_ttf-devel-2.24.0-VC.zip" -o "sdl_ttf.zip"
+  
+  Write-Host -ForegroundColor "blue" "Extracting SDL2_ttf..."
+  Expand-Archive -Path "sdl_ttf.zip" -DestinationPath "SDL_ttf"
+
   Move-Item "SDL2/SDL2-2.30.11/lib/x64" "SDL2_2"
   Remove-Item sdl2.zip
   Remove-Item SDL2 -Recurse
   Rename-Item SDL2_2 SDL2
 
+  Move-Item "SDL_ttf/SDL2_ttf-2.24.0/lib/x64" "SDL_ttf_2"
+  Remove-Item sdl_ttf.zip
+  Remove-Item SDL_ttf -Recurse
+  Rename-Item SDL_ttf_2 SDL_ttf
+
   Set-Location ..
   Move-Item temp\SDL2 build
-  Remove-Item temp
 
   Move-Item build\SDL2\* build
   Remove-Item build\SDL2
+
+  Move-Item temp\SDL_ttf build
+  Move-Item build\SDL_ttf\* build
+  Remove-Item build\SDL_ttf
+  
+  Remove-Item temp -Recurse
 }
 
 # Now we can build
-Write-Host -ForegroundColor "blue" "Building project..."
+Write-Host -ForegroundColor "blue" "Assembling..."
 nasm -fwin64 sedit\sedit.asm -o build\sedit.obj
 
 Write-Host -ForegroundColor "blue" "Linking..."
-GoLink build\sedit.obj /console build\SDL2.dll msvcrt.dll kernel32.dll
+$null = GoLink build\sedit.obj /console build\SDL2.dll msvcrt.dll kernel32.dll
 
 Write-Host -ForegroundColor "green" "Build finished!"
